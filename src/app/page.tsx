4fc432f3dfb97e8ffff2d10776446e23c92c4f5e
Andriy Romanov <mgm.agro04@gmail.com>
2025-10-08 01:48:50 +0300

    edit style header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import styles from './page.module.css';
-import Loader from '@/components/Loader/Loader';
 
 export default function HomePage() {
   const router = useRouter();
 
   return (
     <section className={styles.hero}>
-      <Loader />
       <div className={styles.herocontent}>
         <h1 className={styles.title}>Campers of your dreams</h1>
         <h2 className={styles.subject}>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,3 @@
-/*import Link from 'next/link';
-import styles from './Header.module.css';
-import Image from 'next/image';
-
-export default function Header() {
-  return (
-    <header className={styles.header}>
-      <Image
-        src="/logo.svg"
-        alt="TravelTrucks logo"
-        width={136}
-        height={16}
-        priority // логотип вантажиться першим
-      />
-      <nav
-        className={styles.headernav}
-        style={{ display: 'flex', gap: '20px' }}
-      >
-        <Link href="/">Home</Link>
-        <Link href="/catalog">Catalog</Link>
-      </nav>
-    </header>
-  );
-}*/
 'use client';
 import Link from 'next/link';
 import styles from './Header.module.css';
@@ -32,34 +8,33 @@ export default function Header() {
   const pathname = usePathname();
 
   return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <Link href="/" className={styles.logo}>
-          <Image
-            src="/logo.svg"
-            alt="TravelTrucks logo"
-            width={136}
-            height={16}
-            priority // логотип вантажиться першим
-          />
+    <div className={styles.header}>
+      <Link href="/">
+        <Image
+          src="/logo.svg"
+          alt="TravelTrucks logo"
+          width={136}
+          height={16}
+          className={styles.logo}
+          priority
+        />
+      </Link>
+      <nav className={styles.nav}>
+        <Link
+          href="/"
+          className={`${styles.link} ${pathname === '/' ? styles.active : ''}`}
+        >
+          Home
         </Link>
-        <nav className={styles.nav}>
-          <Link
-            href="/"
-            className={`${styles.link} ${pathname === '/' ? styles.active : ''}`}
-          >
-            Home
-          </Link>
-          <Link
-            href="/catalog"
-            className={`${styles.link} ${
-              pathname.startsWith('/catalog') ? styles.active : ''
-            }`}
-          >
-            Catalog
-          </Link>
-        </nav>
-      </div>
-    </header>
+        <Link
+          href="/catalog"
+          className={`${styles.link} ${
+            pathname.startsWith('/catalog') ? styles.active : ''
+          }`}
+        >
+          Catalog
+        </Link>
+      </nav>
+    </div>
   );
 }
